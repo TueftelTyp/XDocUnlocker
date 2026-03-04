3d6f1ef7304fc8d4d788bf6a7d21f5e7a8c41ed2
Tüftel Typ <31390221+TueftelTyp@users.noreply.github.com>
2026-03-04 12:13:53 +0100

    Add files via upload

---
+++ Code/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace XDocUnlocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            BtnSelectFile = new Button();
+            BtnUnlock = new Button();
+            tbPath = new TextBox();
+            openFileDialog1 = new OpenFileDialog();
+            cbBackup = new CheckBox();
+            btnClose = new Button();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            progressBar1 = new ProgressBar();
+            statusStrip1 = new StatusStrip();
+            lblStatus = new ToolStripStatusLabel();
+            btnPin = new Button();
+            btnMini = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // BtnSelectFile
+            // 
+            BtnSelectFile.Location = new Point(328, 62);
+            BtnSelectFile.Name = "BtnSelectFile";
+            BtnSelectFile.Size = new Size(75, 23);
+            BtnSelectFile.TabIndex = 0;
+            BtnSelectFile.Text = "choose File";
+            BtnSelectFile.UseVisualStyleBackColor = true;
+            BtnSelectFile.Click += BtnSelectFile_Click;
+            // 
+            // BtnUnlock
+            // 
+            BtnUnlock.Enabled = false;
+            BtnUnlock.Location = new Point(328, 91);
+            BtnUnlock.Name = "BtnUnlock";
+            BtnUnlock.Size = new Size(75, 23);
+            BtnUnlock.TabIndex = 1;
+            BtnUnlock.Text = "Unlock";
+            BtnUnlock.UseVisualStyleBackColor = true;
+            BtnUnlock.Click += BtnUnlock_Click;
+            // 
+            // tbPath
+            // 
+            tbPath.Location = new Point(12, 62);
+            tbPath.Name = "tbPath";
+            tbPath.Size = new Size(310, 23);
+            tbPath.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.Filter = "Office (*.docx;*.xlsx;*.pptx)|*.docx;*.xlsx;*.pptx";
+            // 
+            // cbBackup
+            // 
+            cbBackup.AutoSize = true;
+            cbBackup.Checked = true;
+            cbBackup.CheckState = CheckState.Checked;
+            cbBackup.Location = new Point(12, 94);
+            cbBackup.Name = "cbBackup";
+            cbBackup.Size = new Size(65, 19);
+            cbBackup.TabIndex = 4;
+            cbBackup.Text = "Backup";
+            cbBackup.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            btnClose.BackColor = Color.Transparent;
+            btnClose.BackgroundImage = Properties.Resources.exit;
+            btnClose.BackgroundImageLayout = ImageLayout.Zoom;
+            btnClose.FlatAppearance.BorderSize = 0;
+            btnClose.FlatStyle = FlatStyle.Flat;
+            btnClose.ForeColor = Color.Transparent;
+            btnClose.Location = new Point(384, 5);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(19, 19);
+            btnClose.TabIndex = 5;
+            btnClose.UseVisualStyleBackColor = false;
+            btnClose.Click += btnClose_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.xdoclogo;
+            pictureBox1.Location = new Point(12, 5);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(34, 30);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10F);
+            label1.Location = new Point(52, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 19);
+            label1.TabIndex = 7;
+            label1.Text = "XDocUnlocker";
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(328, 91);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(75, 22);
+            progressBar1.TabIndex = 8;
+            progressBar1.Visible = false;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { lblStatus });
+            statusStrip1.Location = new Point(0, 128);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(415, 22);
+            statusStrip1.SizingGrip = false;
+            statusStrip1.Stretch = false;
+            statusStrip1.TabIndex = 9;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(241, 17);
+            lblStatus.Text = "💡 Drag 'n drop Office files or use the button";
+            lblStatus.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // btnPin
+            // 
+            btnPin.BackColor = Color.Transparent;
+            btnPin.BackgroundImage = Properties.Resources.unpinned;
+            btnPin.BackgroundImageLayout = ImageLayout.Zoom;
+            btnPin.FlatAppearance.BorderSize = 0;
+            btnPin.FlatStyle = FlatStyle.Flat;
+            btnPin.ForeColor = Color.Transparent;
+            btnPin.Location = new Point(334, 5);
+            btnPin.Name = "btnPin";
+            btnPin.Size = new Size(19, 19);
+            btnPin.TabIndex = 10;
+            btnPin.UseVisualStyleBackColor = false;
+            btnPin.Click += btnPin_Click;
+            // 
+            // btnMini
+            // 
+            btnMini.BackColor = Color.Transparent;
+            btnMini.BackgroundImage = Properties.Resources.mini;
+            btnMini.BackgroundImageLayout = ImageLayout.Zoom;
+            btnMini.FlatAppearance.BorderSize = 0;
+            btnMini.FlatStyle = FlatStyle.Flat;
+            btnMini.ForeColor = Color.Transparent;
+            btnMini.Location = new Point(359, 5);
+            btnMini.Name = "btnMini";
+            btnMini.Size = new Size(19, 19);
+            btnMini.TabIndex = 11;
+            btnMini.UseVisualStyleBackColor = false;
+            btnMini.Click += btnMini_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(415, 150);
+            Controls.Add(btnMini);
+            Controls.Add(btnPin);
+            Controls.Add(statusStrip1);
+            Controls.Add(progressBar1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnClose);
+            Controls.Add(cbBackup);
+            Controls.Add(tbPath);
+            Controls.Add(BtnUnlock);
+            Controls.Add(BtnSelectFile);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "XDocUnlocker";
+            MouseDown += Form1_MouseDown;
+            MouseMove += Form1_MouseMove;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BtnSelectFile;
+        private Button BtnUnlock;
+        private TextBox tbPath;
+        private OpenFileDialog openFileDialog1;
+        private CheckBox cbBackup;
+        private Button btnClose;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private ProgressBar progressBar1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel lblStatus;
+        private Button btnPin;
+        private Button btnMini;
+    }
+}
---
+++ Code/Form1.cs
@@ -0,0 +1,453 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace XDocUnlocker
+{
+    public partial class Form1 : Form
+    {
+        private string? selectedFilePath = "";
+        private Point _mouseDownPos;
+        private bool _isPinned = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += Form1_DragEnter;
+            this.DragDrop += Form1_DragDrop;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbBackup.Checked = true;
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            progressBar1.Maximum = 100;
+            progressBar1.Visible = false;
+
+            lblStatus.Text = "💡 Drag & drop Office files or use the button";
+            lblStatus.ForeColor = System.Drawing.Color.Blue;
+        }
+
+        private void Form1_DragEnter(object? sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                if (e.Data.GetData(DataFormats.FileDrop) is string[] files && files.Length > 0)
+                {
+                    bool isOfficeFile = files.Any(f =>
+                        f?.EndsWith(".docx", StringComparison.OrdinalIgnoreCase) == true ||
+                        f?.EndsWith(".xlsx", StringComparison.OrdinalIgnoreCase) == true ||
+                        f?.EndsWith(".pptx", StringComparison.OrdinalIgnoreCase) == true);
+
+                    e.Effect = isOfficeFile ? DragDropEffects.Copy : DragDropEffects.None;
+                }
+            }
+        }
+
+        private void Form1_DragDrop(object? sender, DragEventArgs e)
+        {
+            if (e.Data.GetData(DataFormats.FileDrop) is string[] files && files.Length > 0)
+            {
+                var officeFiles = files
+                    .Where(f => !string.IsNullOrEmpty(f) &&
+                        (f.EndsWith(".docx", StringComparison.OrdinalIgnoreCase) ||
+                         f.EndsWith(".xlsx", StringComparison.OrdinalIgnoreCase) ||
+                         f.EndsWith(".pptx", StringComparison.OrdinalIgnoreCase)))
+                    .ToArray();
+
+                if (officeFiles.Length > 0)
+                {
+                    selectedFilePath = officeFiles[0];
+                    tbPath.Text = string.Join(" | ", officeFiles);
+                    tbPath.SelectionStart = tbPath.Text.Length;
+                    tbPath.SelectionLength = 0;
+                    tbPath.ScrollToCaret();
+
+                    BtnUnlock.Enabled = true;
+                    lblStatus.Text = $"✅ {officeFiles.Length} Office file(s) loaded. First: '{Path.GetFileName(officeFiles[0])}'";
+                    if (officeFiles.Length > 1)
+                        lblStatus.Text += $" ({officeFiles.Length - 1} more ready)";
+                    lblStatus.ForeColor = System.Drawing.Color.Green;
+                }
+                else
+                {
+                    lblStatus.Text = "❌ No valid Office files (.docx/.xlsx/.pptx) found!";
+                    lblStatus.ForeColor = System.Drawing.Color.Orange;
+                }
+            }
+        }
+
+        private void BtnSelectFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Filter = "Office (*.docx;*.xlsx;*.pptx)|*.docx;*.xlsx;*.pptx";
+                dlg.Multiselect = true;
+                dlg.Title = "Choose Locked Office File(s)";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    var officeFiles = dlg.FileNames
+                        .Where(f => !string.IsNullOrEmpty(f) &&
+                            (f.EndsWith(".docx", StringComparison.OrdinalIgnoreCase) ||
+                             f.EndsWith(".xlsx", StringComparison.OrdinalIgnoreCase) ||
+                             f.EndsWith(".pptx", StringComparison.OrdinalIgnoreCase)))
+                        .ToArray();
+
+                    if (officeFiles.Length > 0)
+                    {
+                        selectedFilePath = officeFiles[0];
+                        tbPath.Text = string.Join(" | ", officeFiles);
+                        tbPath.SelectionStart = tbPath.Text.Length;
+                        tbPath.SelectionLength = 0;
+                        tbPath.ScrollToCaret();
+
+                        BtnUnlock.Enabled = true;
+                        lblStatus.Text = $"✅ {officeFiles.Length} Office file(s) loaded. First: '{Path.GetFileName(officeFiles[0])}'";
+                        if (officeFiles.Length > 1)
+                            lblStatus.Text += $" ({officeFiles.Length - 1} more ready)";
+                        lblStatus.ForeColor = System.Drawing.Color.Green;
+                    }
+                }
+            }
+        }
+
+        private void tbPath_TextChanged(object sender, EventArgs e)
+        {
+            selectedFilePath = tbPath.Text;
+        }
+
+        private async void BtnUnlock_Click(object sender, EventArgs e)
+        {
+            string[] allInputFiles = tbPath.Text.Split(new char[] { '|', ',', ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(f => f.Trim())
+                .Where(f => !string.IsNullOrEmpty(f) && File.Exists(f))
+                .Where(f => f.EndsWith(".docx", StringComparison.OrdinalIgnoreCase) ||
+                           f.EndsWith(".xlsx", StringComparison.OrdinalIgnoreCase) ||
+                           f.EndsWith(".pptx", StringComparison.OrdinalIgnoreCase))
+                .ToArray();
+
+            if (allInputFiles.Length == 0)
+            {
+                lblStatus.Text = "❌ No valid Office files found!";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+                return;
+            }
+
+            BtnUnlock.Enabled = false;
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            progressBar1.Maximum = allInputFiles.Length * 100;
+            progressBar1.Value = 0;
+            progressBar1.Visible = true;
+            lblStatus.Text = $"🚀 Unlocking {allInputFiles.Length} file(s)...";
+            lblStatus.ForeColor = System.Drawing.Color.Orange;
+            await Task.Delay(500);
+            Application.DoEvents();
+
+            try
+            {
+                int totalProgress = 0;
+                int successCount = 0;
+
+                foreach (string filePath in allInputFiles)
+                {
+                    try
+                    {
+                        lblStatus.Text = $"🔓 [{successCount + 1}/{allInputFiles.Length}] {Path.GetFileName(filePath)}...";
+                        Application.DoEvents();
+
+                        string unlockedPath = Path.Combine(
+                            Path.GetDirectoryName(filePath) ?? "",
+                            Path.GetFileNameWithoutExtension(filePath) + "_unlocked" +
+                            Path.GetExtension(filePath));
+
+                        string? backupPath = null;
+                        if (cbBackup.Checked)
+                        {
+                            lblStatus.Text = $"📦 Backup: {Path.GetFileName(filePath)}...";
+                            backupPath = filePath + ".backup";
+                            await Task.Run(() => File.Copy(filePath, backupPath!, true));
+                        }
+
+                        var singleProgress = (IProgress<int>?)null;
+                        await RemoveProtectionWithProgress(filePath, unlockedPath, singleProgress);
+
+                        successCount++;
+                        totalProgress += 100;
+                        progressBar1.Value = totalProgress;
+
+                        lblStatus.Text = $"✅ [{successCount}/{allInputFiles.Length}] {Path.GetFileName(filePath)} done";
+                        lblStatus.ForeColor = System.Drawing.Color.Green;
+                        await Task.Delay(200);
+                        Application.DoEvents();
+                    }
+                    catch (Exception fileEx)
+                    {
+                        lblStatus.Text = $"⚠️ [{successCount + 1}/{allInputFiles.Length}] ERROR {Path.GetFileName(filePath)}: {fileEx.Message}";
+                        lblStatus.ForeColor = System.Drawing.Color.Orange;
+                        await Task.Delay(1000);
+                        Application.DoEvents();
+                    }
+                }
+
+                lblStatus.Text = $"✅ COMPLETE! {successCount}/{allInputFiles.Length} file(s) successfully unlocked!";
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+                await Task.Delay(5000);
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = $"❌ CRITICAL ERROR: {ex.Message}";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+                await Task.Delay(3000);
+            }
+            finally
+            {
+                progressBar1.Visible = false;
+                BtnUnlock.Enabled = true;
+                Application.DoEvents();
+            }
+        }
+
+        private async Task RemoveProtectionWithProgress(string inputPath, string outputPath, IProgress<int>? progress)
+        {
+            string tempDir = Path.Combine(Path.GetTempPath(), "XDocUnlocker_" + Guid.NewGuid().ToString("N")[..8]);
+            Directory.CreateDirectory(tempDir);
+
+            try
+            {
+                progress?.Report(10);
+                ZipFile.ExtractToDirectory(inputPath, tempDir);
+
+                string ext = Path.GetExtension(inputPath).ToLowerInvariant();
+                int totalSteps = GetFileCount(tempDir, ext);
+                int stepSize = 80 / Math.Max(1, totalSteps);
+
+                switch (ext)
+                {
+                    case ".xlsx": await ProcessExcelAsync(tempDir, progress, stepSize); break;
+                    case ".docx": await ProcessWordAsync(tempDir, progress); break;
+                    case ".pptx": await ProcessPowerPointAsync(tempDir, progress); break;
+                }
+
+                progress?.Report(95);
+                File.Copy(inputPath, outputPath, true);
+                using (var zip = ZipFile.Open(outputPath, ZipArchiveMode.Update))
+                {
+                    PatchZipEntries(tempDir, zip, ext);
+                }
+
+                progress?.Report(100);
+            }
+            finally
+            {
+                if (Directory.Exists(tempDir))
+                    Directory.Delete(tempDir, true);
+            }
+        }
+
+        private static int GetFileCount(string tempDir, string ext)
+        {
+            return ext switch
+            {
+                ".xlsx" => Directory.GetFiles(Path.Combine(tempDir, "xl", "worksheets"), "sheet*.xml").Length,
+                ".docx" => 1,
+                ".pptx" => 1,
+                _ => 1
+            };
+        }
+
+        private async Task ProcessExcelAsync(string tempDir, IProgress<int>? progress, int stepSize)
+        {
+            string xlDir = Path.Combine(tempDir, "xl", "worksheets");
+            if (Directory.Exists(xlDir))
+            {
+                var sheets = Directory.GetFiles(xlDir, "sheet*.xml");
+                int current = 10;
+                foreach (string sheet in sheets)
+                {
+                    RemoveProtectionTag(sheet, "sheetProtection");
+                    current += stepSize;
+                    progress?.Report(Math.Min(90, current));
+                    await Task.Delay(10);
+                }
+            }
+        }
+
+        private async Task ProcessWordAsync(string tempDir, IProgress<int>? progress)
+        {
+            string wordSettings = Path.Combine(tempDir, "word", "settings.xml");
+            if (File.Exists(wordSettings))
+            {
+                RemoveProtectionTag(wordSettings, "w:documentProtection");
+                progress?.Report(90);
+            }
+            await Task.Delay(10);
+        }
+
+        private async Task ProcessPowerPointAsync(string tempDir, IProgress<int>? progress)
+        {
+            string pptFile = Path.Combine(tempDir, "ppt", "presentation.xml");
+            if (File.Exists(pptFile))
+            {
+                RemoveProtectionTag(pptFile, "p:modifyVerifier");
+                progress?.Report(90);
+            }
+            await Task.Delay(10);
+        }
+
+        private static void PatchZipEntries(string tempDir, ZipArchive zip, string ext)
+        {
+            switch (ext)
+            {
+                case ".xlsx":
+                    string xlDir = Path.Combine(tempDir, "xl", "worksheets");
+                    if (Directory.Exists(xlDir))
+                    {
+                        foreach (string sheet in Directory.GetFiles(xlDir, "sheet*.xml"))
+                        {
+                            string entryName = "xl/worksheets/" + Path.GetFileName(sheet);
+                            var entry = zip.GetEntry(entryName);
+                            entry?.Delete();
+                            zip.CreateEntryFromFile(sheet, entryName, CompressionLevel.Optimal);
+                        }
+                    }
+                    break;
+                case ".docx":
+                    string wordSettings = Path.Combine(tempDir, "word", "settings.xml");
+                    if (File.Exists(wordSettings))
+                    {
+                        string entryName = "word/settings.xml";
+                        var entry = zip.GetEntry(entryName);
+                        entry?.Delete();
+                        zip.CreateEntryFromFile(wordSettings, entryName, CompressionLevel.Optimal);
+                    }
+                    break;
+                case ".pptx":
+                    string pptFile = Path.Combine(tempDir, "ppt", "presentation.xml");
+                    if (File.Exists(pptFile))
+                    {
+                        string entryName = "ppt/presentation.xml";
+                        var entry = zip.GetEntry(entryName);
+                        entry?.Delete();
+                        zip.CreateEntryFromFile(pptFile, entryName, CompressionLevel.Optimal);
+                    }
+                    break;
+            }
+        }
+
+        private static void RemoveProtectionTag(string xmlFile, string tagName)
+        {
+            try
+            {
+                var doc = new XmlDocument();
+                doc.PreserveWhitespace = true;
+                doc.Load(xmlFile);
+
+                var ns = new XmlNamespaceManager(doc.NameTable);
+                ns.AddNamespace("x", "http://schemas.openxmlformats.org/spreadsheetml/2006/main");
+                ns.AddNamespace("w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main");
+                ns.AddNamespace("p", "http://schemas.openxmlformats.org/presentationml/2006/main");
+                ns.AddNamespace("a", "http://schemas.openxmlformats.org/drawingml/2006/main");
+                ns.AddNamespace("r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships");
+
+                if (tagName == "sheetProtection")
+                {
+                    var sheets = doc.SelectNodes("//x:sheetProtection", ns);
+                    if (sheets != null)
+                    {
+                        foreach (XmlNode node in sheets)
+                            node.ParentNode?.RemoveChild(node);
+                    }
+                }
+                else if (tagName == "w:documentProtection")
+                {
+                    var prot = doc.SelectNodes("//w:documentProtection", ns);
+                    if (prot != null)
+                    {
+                        foreach (XmlNode node in prot)
+                        {
+                            if (node.Attributes?["w:enforcement"] is XmlAttribute enforce)
+                                enforce.Value = "0";
+                        }
+                    }
+                }
+                else if (tagName == "p:modifyVerifier")
+                {
+                    var mods = doc.SelectNodes("//p:modifyVerifier", ns);
+                    if (mods != null)
+                    {
+                        foreach (XmlNode node in mods)
+                            node.ParentNode?.RemoveChild(node);
+                    }
+                }
+
+                doc.Save(xmlFile);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error processing {xmlFile}: {ex.Message}");
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            string url = "https://github.com/TueftelTyp/XDocUnlocker";
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (_isPinned) return;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                _mouseDownPos = new Point(e.X, e.Y);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isPinned) return;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                int deltaX = e.X - _mouseDownPos.X;
+                int deltaY = e.Y - _mouseDownPos.Y;
+                this.Location = new Point(Location.X + deltaX, Location.Y + deltaY);
+            }
+        }
+
+        private void btnMini_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnPin_Click(object sender, EventArgs e)
+        {
+            _isPinned = !_isPinned;
+            this.TopMost = _isPinned;
+
+            if (_isPinned)
+            {
+                btnPin.Image = Properties.Resources.unpinned;
+            }
+            else
+            {
+                btnPin.Image = Properties.Resources.pinned;
+            }
+        }
+    }
+}
